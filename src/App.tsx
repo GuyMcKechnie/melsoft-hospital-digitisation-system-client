a32cc32c7d53da1070c4ccb6fb4b9237c2f40f03
GuyMcKechnie <mckechnieguy.mg@gmail.com>
2026-01-27 21:20:53 +0200

    feat(dashboard): restructure dashboard components and add user dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./pages/login";
 import SignupPage from "./pages/signup";
 import ForgotPasswordPage from "./pages/forgot-password";
-import ClinicalDashboardPage from "./pages/dashboard";
+import ClinicalDashboardPage from "./pages/clinical-dashboard-page";
+import UserDashboard from "./pages/user-dashboard";
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
             <Route path="/signup" element={<SignupPage />} />
             <Route path="/forgot-password" element={<ForgotPasswordPage />} />
             <Route path="/dashboard" element={<ClinicalDashboardPage />} />
+            <Route path="/user-dashboard" element={<UserDashboard />} />
             <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
     );
